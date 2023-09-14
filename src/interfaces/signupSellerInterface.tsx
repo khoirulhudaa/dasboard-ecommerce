a103bd074a3e62ce04158b717b33a38770f0c7ab
valclassdevelop <developervalclass.com>
2023-09-14 07:53:30 +0700

    create signin seller electshop

--- src/interfaces/signupSellerInterface.tsx
+++ src/interfaces/signupSellerInterface.tsx
@@ -1,7 +1,7 @@
 export interface signupSellerInterface {
-    email_seller: String,
-    seller_name: String,
-    gender: String,
-    telephone_seller: String,
-    password: String
+    email_seller?: String,
+    seller_name?: String,
+    gender?: String,
+    telephone_seller?: String,
+    password?: String
 }
