fcec6e46a63cfc353b2ff599cf2141727d5a3663
valclassdevelop <developervalclass.com>
2023-09-21 10:51:18 +0700

    get token in axios interceptors

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -1,44 +1,39 @@
 import axios from "axios";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import store from "../store";
 
-const navigate = useNavigate()
+const api = axios.create({
+  baseURL: 'https://huda.descode.id',
+  headers: {
+    "Content-Type": "application/json"
+  }    
+})
 
-const setupAxiosInterceptors = () => {
-  const api = axios.create({
-      baseURL: 'https://huda.descode.id',
-      headers: {
-        "Content-Type": "application/json"
-      }    
-  })
+// Add a request interceptor
+api.interceptors.request.use(function (config) {
+  const Store = store.getState()
+  const token = Store.authSlice.token
 
-  // Add a request interceptor
-  api.interceptors.request.use(function (config) {
-      const token = useSelector((state: any) => state.authSlice.token)
-      
-      if(token !== "") {
-        console.log('config', config)
-        config.headers.Authorization = `Bearer ${token}`
-      }
-      return config;
+  if(token !== "") {
+    console.log('config', config)
+    config.headers.Authorization = `Bearer ${token}`
+  }
 
-    }, function (error) {
-      return Promise.reject(error);
-  });
+  return config;
 
-  // Add a response interceptor
-  api.interceptors.response.use(function (response) {
-      return response;
-    }, function (error) {
+}, function (error) {
+  return Promise.reject(error);
+});
 
-      if(error.response && error.response.status === 401) {
-        navigate('/')
-      }
-      return Promise.reject(error);
-  });
+// Add a response interceptor
+api.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
 
-  return api
+  if(error.response && error.response.status === 401) {
+    return Promise.reject(error);
+  }
+});
 
-}
 
-export default setupAxiosInterceptors
+
+export default api
